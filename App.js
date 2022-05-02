10fc0c73e8c775fa3f490ca568ac28e74d327a46
MrGuissey <gabriel.bourgeois.email@protonmail.com>
2022-05-02 14:11:15 +0200

    commit test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome on cookyt</Text>
+      <Text>Welcome on cookyt fun fun fun</Text>
       <StatusBar style="auto" />
     </View>
   );
