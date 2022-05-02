e02b94050de358a1fcea2d818adaa83031346ebe
MrGuissey <gabriel.bourgeois.email@protonmail.com>
2022-05-02 14:13:14 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome on cookyt</Text>
+      <Text>Welcome on cookyt trolo</Text>
       <StatusBar style="auto" />
     </View>
   );
