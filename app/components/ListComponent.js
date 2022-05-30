b185b98c9b7e291cccdcfee343ffdf87a31ffb63
louise <louise.poughet@etu.utc.fr>
2022-05-30 14:59:47 +0200

    navigation to recipes list

--- app/components/ListComponent.js
+++ app/components/ListComponent.js
@@ -3,6 +3,7 @@ import { Component } from "react"
 import React from 'react'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../Styles'
+import { log } from 'react-native-reanimated';
 
 
 const { height, width } = Dimensions.get('window')
@@ -13,18 +14,20 @@ function ListComponent (props){
   const backgroundColor = props.backgroundColor; 
   const fontcolor = props.fontcolor; 
   const liste = props.liste;
+  //const target = props.target;
+
+  console.log("DOESN NOT WORK *****************************************************************");
 
         return(
-          <TouchableOpacity onPress={()=> navigation.navigate('RecipesListScreen',{liste: liste})} style={{width: 350}}>
+          <TouchableOpacity onPress={()=> navigation.navigate("RecipesListScreen",{liste: liste})} style={{width: 350}}>
             <View style={{flexDirection:'row', justifyContent:'center',backgroundColor:backgroundColor, height: 60, borderRadius:20, marginLeft: 20, marginBottom:5, marginRight:20}}>
                   <View style={{justifyContent:'center',width:145, marginLeft:10, flex:1}}>
-                    <Text style={[{color:fontcolor, justifyContent:'center'}, styles.h2]} >{liste.libraryname}</Text>
+                    <Text style={[{color:fontcolor, justifyContent:'center'}, styles.h2]} >{liste.name}</Text>
                   </View>
                 </View>
            </TouchableOpacity>
         )
 
-    
 }
 
 export default ListComponent; 
